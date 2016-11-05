8710318687d8628e8dbc7bfba10cc15a44780d6e
Mariano Gappa <mariano@movio.co>
2016-11-05 19:49:49 +1300

    Enables cpuprofile option.

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	port := mustResolvePort(41234)
 	listener := mustGetListener(port)
 	baseTemplate := mustParseBasePageTemplate()
