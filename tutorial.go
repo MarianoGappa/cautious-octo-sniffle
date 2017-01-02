56015c55064675e155735699b7e030bda5271abe
Mariano Gappa <mariano@movio.co>
2017-01-03 10:48:05 +1300

    Implements goto & repeat for tutorial.

--- tutorial.go
+++ tutorial.go
@@ -15,29 +15,42 @@ func tutorial() chan *sarama.ConsumerMessage {
 }
 
 func pushTutorialMessages(c chan *sarama.ConsumerMessage) {
-	for _, e := range tutorialEvents() {
-		switch e.kind {
-		case "sleep":
-			time.Sleep(e.duration)
-		case "message":
-			c <- e.message
+	es := tutorialEvents()
+	i := 0
+	for {
+		if i == len(es) {
+			break
 		}
+		switch es[i].kind {
+		case "sleep":
+			time.Sleep(es[i].duration)
+		case "message":
+			c <- es[i].message
+		}
+		if es[i].repeat > 0 {
+			es[i].repeat -= 1
+			i += es[i].jumpRelative
+			continue
+		}
+		i++
 	}
+}
+
+type tutorialEvent struct {
+	kind         string
+	duration     time.Duration
+	jumpRelative int
+	repeat       int
+	message      *sarama.ConsumerMessage
 }
 
 func tutorialEvents() []tutorialEvent {
 	return []tutorialEvent{
 		{kind: "message", message: &sarama.ConsumerMessage{Topic: "one", Value: []byte("{}")}},
-		{kind: "sleep", duration: 5 * time.Second},
+		{kind: "sleep", duration: 1 * time.Second},
 		{kind: "message", message: &sarama.ConsumerMessage{Topic: "one", Value: []byte("{}")}},
-		{kind: "sleep", duration: 5 * time.Second},
+		{kind: "sleep", duration: 1 * time.Second},
 		{kind: "message", message: &sarama.ConsumerMessage{Topic: "one", Value: []byte("{}")}},
-		{kind: "sleep", duration: 5 * time.Second},
+		{kind: "sleep", duration: 1 * time.Second, jumpRelative: -5, repeat: 2},
 	}
-}
-
-type tutorialEvent struct {
-	kind     string
-	duration time.Duration
-	message  *sarama.ConsumerMessage
 }
